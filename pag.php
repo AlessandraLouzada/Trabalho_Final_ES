e20f0235d71db6e5218b26607d03293d0207c6a0
AlessandraLouzada <54905001+AlessandraLouzada@users.noreply.github.com>
2019-10-05 16:22:32 -0300

    Create pag.php

---
+++ pag.php
@@ -0,0 +1,17 @@
+<?php
+
+$nome= $_POST['fname'];
+$cpf= $_POST['cpf'];
+$nasc= $_POST['dnasc'];
+$email= $_POST['demail'];
+$estado= $_POST['cestado'];
+$tel= $_POST['cfone'];
+$dep= 'DCC';
+$salario= $_POST['csalario'];
+$carro= $_POST['ccarro'];
+
+
+echo "$nome <br>"."$cpf <br>"."$nasc <br>"."$email<br>"."$estado<br>";
+echo "$tel<br>"."$dep<br>"."$salario<br>"."$carro<br>";
+
+?>
